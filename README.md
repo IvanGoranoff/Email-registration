36c0d049ddd90f6fa0073548e43ba0bcbc1c4f95
IvanGoranoff <94969526+IvanGoranoff@users.noreply.github.com>
2021-12-12 17:42:06 +0200

    Email registration
    Email registration form. I used https://www.tutsmake.com/user-registration-with-email-verification-in-php/ article.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Email registration
- Email registration done by reading https://www.tutsmake.com/user-registration-with-email-verification-in-php/ .
+# 1stEmail
+ Email Registration
---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+	$servername='localhost';
+	$username='root';
+	$password='';
+	$tel='';
+	$dbname = "my_db";
+	$conn=mysqli_connect($servername,$username,$password,"$dbname");
+	  
+?>
